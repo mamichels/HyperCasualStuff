6cc6021f1d78a0253cabf3c91e8611430e48a34c
Philip Maaß <PhilipMaasz@aol.com>
2020-01-18 17:49:25 +0100

    refactors game loop service

--- src/game-loop.service.ts
+++ src/game-loop.service.ts
@@ -5,11 +5,7 @@ export class GameLoopService {
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
 
-    constructor() {
-
-    }
-
-    public initLoop(): void{
+    public initLoop(): void {
         if(document.querySelector('canvas')){
             this.canvas = document.querySelector('canvas');
             this.context = this.canvas.getContext('2d');
@@ -18,13 +14,14 @@ export class GameLoopService {
         }
     }
 
-    private draw(): void  {
+    private draw(): void {
         // Clear the Canvas
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.clearRect(0, 0, this.canvas?.width, this.canvas?.height);
+        // Sets background color of context
         this.context.fillStyle = "red";
 
         // Draw the background
-        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillRect(0, 0, this.canvas?.width, this.canvas?.height);
 
         requestAnimationFrame(this.draw);
     }
