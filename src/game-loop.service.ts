14a1daec07bea31953c1fdd4ac532ccdc48985f1
Philip Maaß <PhilipMaasz@aol.com>
2020-01-18 17:06:11 +0100

    draw function uses requestAnimationFrame function and draws red square

--- src/game-loop.service.ts
+++ src/game-loop.service.ts
@@ -2,8 +2,17 @@ import {injectable} from "tsyringe";
 
 @injectable()
 export class GameLoopService {
+    canvas = document.querySelector('canvas');
+    context = this.canvas.getContext('2d');
 
-    public draw(): void {
-        console.log('MArcel stinkt!');
+    public draw(): void  {
+        // Clear the Canvas
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = "red";
+
+        // Draw the background
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        requestAnimationFrame(() => this.draw());
     }
 }
